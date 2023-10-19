18ed72d7e99e7669b77f1052e3a5e020fcd3364d
Roman Tarasenko <tarrim80@yandex.ru>
2023-10-19 23:05:23 +0600

    Add model URLMap

--- yacut/models.py
+++ yacut/models.py
@@ -4,4 +4,20 @@ from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.Text, unique=True, nullable=False)
+    short = db.Column(db.String(16), nullable=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    # def to_dict(self):
+    #     return dict(
+    #         id=self.id,
+    #         short=self.short,
+    #         original=self.original,
+    #         timestamp=self.timestamp,
+    #     )
+
+    # def from_dict(self, data):
+    #     for field in ("short", "original"):
+    #         if field in data:
+    #             setattr(self, field, data[field])
