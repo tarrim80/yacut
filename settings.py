de259d2feeaaf08047860cfa061eabdde248b97f
Roman Tarasenko <tarrim80@yandex.ru>
2023-10-23 15:45:57 +0600

    Update settins.py

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"  # С подгрузкой из .env тест на платформе не проходит
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
